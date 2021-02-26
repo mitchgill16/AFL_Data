07daaf8c5396da0a607a96f8850426fea1a59944
Mitchell Gill <mitchgill16@gmail.com>
2021-02-26 21:25:36 +0800

    best model saved for feature extraction

--- create_ml_model.py
+++ create_ml_model.py
@@ -9,7 +9,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from random import randint
 from Gather_AFL_Data import gatherer as gad
-from fdnn import feature_extractor as fex
+#from fdnn import feature_extractor as fex
 import skopt
 from skopt.searchcv import BayesSearchCV
 from skopt.space import Real, Categorical, Integer
