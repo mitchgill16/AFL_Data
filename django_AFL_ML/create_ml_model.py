60a3ea104f49aa169a77bf78b2038fb12ca187d3
mitchgill16 <mitchgill16@gmail.com>
2021-07-27 15:35:18 +0800

    round 20 2021

--- django_AFL_ML/create_ml_model.py
+++ django_AFL_ML/create_ml_model.py
@@ -443,8 +443,8 @@ def main():
     #best_model = predict_margin(x_data, margin_label, dnn_model, ohe, teams)
 
     # model = pickle.load(open("xgb_model.dat", "rb"))
-    games = [13,4,5,9,7,14,3,12,2,8,17,15,1,10,11,18,16,6]
-    round = 19
+    games = [15,3,18,1,12,7,8,11,4,17,5,16,10,2,9,13,6,14]
+    round = 20
     pda, mda, best_xgb = run_predictions(x_data, y_label, win_model, margin_label, margin_model, ohe, teams, games, round)
     print(pda)
     print(mda)
