6a91ed46ada381db3b812c391d4ab35dfc90a8ed
mitchgill16 <mitchgill16@gmail.com>
2022-05-26 17:30:29 +0800

    PAV updates and round 11 preds

--- django_AFL_ML/run_df.sh
+++ django_AFL_ML/run_df.sh
@@ -1,16 +1,16 @@
 #uncomment below 2 lines, make this an argument to make this easier.
 
-Rscript R_Code/update.R 2022 8
+Rscript R_Code/update.R 2022 10
 
-python clean_team_data.py 10607 10615
+python clean_team_data.py 10625 10633
 
-python assemble_df.py 10607 10615 2
-python assemble_df.py 10607 10615 10
+python assemble_df.py 10625 10633 2
+python assemble_df.py 10625 10633 10
 
-python assemble_df.py 10607 10615 3
-python assemble_df.py 10615 4
-python assemble_df.py 10615 5
-python assemble_df.py 10615 6
-python assemble_df.py 10615 7
-python assemble_df.py 10615 8
-python assemble_df.py 10615 9
+python assemble_df.py 10625 10633 3
+#python assemble_df.py 10615 4
+#python assemble_df.py 10615 5
+#python assemble_df.py 10615 6
+#python assemble_df.py 10615 7
+#python assemble_df.py 10615 8
+#python assemble_df.py 10615 9
