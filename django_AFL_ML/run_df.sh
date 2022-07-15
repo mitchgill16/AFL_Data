9ad0a90682918f67e1bcac2d361fff6a831e9849
mitchgill16 <mitchgill16@gmail.com>
2022-07-15 15:32:00 +0800

    2022 PAV player updates and round 18 2022 predictions and data updates

--- django_AFL_ML/run_df.sh
+++ django_AFL_ML/run_df.sh
@@ -1,13 +1,13 @@
 #uncomment below 2 lines, make this an argument to make this easier.
 
-Rscript R_Code/update.R 2022 10
+Rscript R_Code/update.R 2022 17
 
-python clean_team_data.py 10625 10633
+python clean_team_data.py 10679 10687
 
-python assemble_df.py 10625 10633 2
-python assemble_df.py 10625 10633 10
+python assemble_df.py 10679 10687 2
+python assemble_df.py 10679 10687 10
 
-python assemble_df.py 10625 10633 3
+python assemble_df.py 10679 10687 3
 #python assemble_df.py 10615 4
 #python assemble_df.py 10615 5
 #python assemble_df.py 10615 6
