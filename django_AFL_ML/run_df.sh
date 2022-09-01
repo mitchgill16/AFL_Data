3cc79ffe0a35e585db152b774cd4b2576c94afe3
mitchgill16 <mitchgill16@gmail.com>
2022-09-02 07:26:54 +0800

    home and away season complete. Predictions for first finals. new model optimised.

--- django_AFL_ML/run_df.sh
+++ django_AFL_ML/run_df.sh
@@ -1,13 +1,14 @@
 #uncomment below 2 lines, make this an argument to make this easier.
 
-Rscript R_Code/update.R 2022 17
+Rscript R_Code/update.R 2022 23
 
-python clean_team_data.py 10679 10687
+python clean_team_data.py 10733 10741
 
-python assemble_df.py 10679 10687 2
-python assemble_df.py 10679 10687 10
+python assemble_df.py 10733 10741 2
+python assemble_df.py 10733 10741 10
+python assemble_df.py 10733 10741 3
 
-python assemble_df.py 10679 10687 3
+
 #python assemble_df.py 10615 4
 #python assemble_df.py 10615 5
 #python assemble_df.py 10615 6
