db88ba9ba245b464a86f64886a24e275917378c3
mitchgill16 <mitchgill16@gmail.com>
2022-09-23 15:35:16 +0800

    2022 GF predictions + deleting old files and renaming folders

--- django_AFL_ML/blog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='blog_home'),
-    path('about/', views.about, name='blog_about'),
-    path('history/', views.history, name='blog_history'),
-    path('discussion/', views.discussion, name='blog_discussion')
-]
